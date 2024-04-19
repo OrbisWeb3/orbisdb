ac86d698e0bc9c5738240f4d500b4fff72050fa2
Donat Andelic <34493754+donatandelic@users.noreply.github.com>
2024-04-19 16:55:06 +0200

    feat(logger): custom util.format and console output

--- server/logger/index.js
+++ server/logger/index.js
@@ -1,5 +1,40 @@
 import * as winston from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
+import util from "util";
+
+import { cliColors } from "../utils/cliColors.js";
+
+function reduceCliColors(colors) {
+  const merged = [];
+
+  for (const color of Object.values(colors)) {
+    if (typeof color === "object") {
+      merged.push(...reduceCliColors(color));
+    } else {
+      merged.push(color);
+    }
+  }
+
+  return merged;
+}
+
+const colorArray = reduceCliColors(cliColors);
+
+const sanitizeColors = (messages) => {
+  const sanitized = [];
+
+  for (const message of messages) {
+    if (colorArray.includes(message)) {
+      continue;
+    }
+
+    sanitized.push(message);
+  }
+
+  return sanitized;
+};
+
+const logsDir = "./server/logs";
 
 const errorFileRotate = new DailyRotateFile({
   level: "error",
@@ -8,7 +43,7 @@ const errorFileRotate = new DailyRotateFile({
   zippedArchive: true,
   maxSize: "20m",
   maxFiles: "5",
-  dirname: "../.logs",
+  dirname: logsDir,
 });
 
 const infoFileRotate = new DailyRotateFile({
@@ -18,7 +53,7 @@ const infoFileRotate = new DailyRotateFile({
   zippedArchive: true,
   maxSize: "10m",
   maxFiles: "1",
-  dirname: "../.logs",
+  dirname: logsDir,
 });
 
 const debugFileRotate = new DailyRotateFile({
@@ -28,20 +63,42 @@ const debugFileRotate = new DailyRotateFile({
   zippedArchive: true,
   maxSize: "10m",
   maxFiles: "1",
-  dirname: "../.logs",
+  dirname: logsDir,
 });
 
-export const logger = winston.createLogger({
+export const _logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ level, message, label, timestamp }) => {
+      return JSON.stringify({
+        level,
+        message: util.format(...sanitizeColors(message)),
+        label,
+        timestamp,
+      });
+    })
+  ),
   transports: [
     errorFileRotate,
     debugFileRotate,
     infoFileRotate,
-    ...((process.env.NODE_ENV !== "production" &&
+    ...((process.env.NODE_ENV !== "production" && [
       new winston.transports.Console({
         level: process.env.LOG_LEVEL || "info",
-      })) ||
+        format: winston.format.printf(({ level, message, label }) => {
+          return `[${level.toUpperCase()}${label ? `:${label}` : ""}] ${util.format(...message)}`;
+        }),
+      }),
+    ]) ||
       []),
   ],
 });
+
+const logger = {
+  info: (...msgs) => _logger.info(msgs),
+  warn: (...warns) => _logger.warn(warns),
+  error: (...errors) => _logger.error(errors),
+  debug: (...msgs) => _logger.debug(msgs),
+};
 
 export default logger;
