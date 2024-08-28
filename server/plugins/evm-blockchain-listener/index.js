282c19bb4b038b19e2fc9c7d8c37f22c37d6c343
Donat Andelic <34493754+donatandelic@users.noreply.github.com>
2024-08-28 12:28:43 +0200

    fix(plugin:evm): unsubscribe error handling

--- server/plugins/evm-blockchain-listener/index.js
+++ server/plugins/evm-blockchain-listener/index.js
@@ -3,6 +3,7 @@ import CustomInterface from "./interfaces/custom/index.js";
 import ERC1155Interface from "./interfaces/erc1155/index.js";
 import ERC20Interface from "./interfaces/erc20/index.js";
 import ERC721Interface from "./interfaces/erc721/index.js";
+import { cliColors } from "../../utils/cliColors.js";
 
 export default class EthereumEventPlugin {
   interface;
@@ -16,18 +17,27 @@ export default class EthereumEventPlugin {
   }
 
   async start() {
-    console.log("Enter start() in EthereumEventPlugin");    
+    console.log("Enter start() in EthereumEventPlugin");
     this.contract_type = "custom";
     // Will pick which interface to use based on the type of smart contract being listened to (ex: ERC20, ERC721 etc)
-    switch(this.contract_type) {
+    switch (this.contract_type) {
       case "erc20":
-        this.interface = new ERC20Interface(this.contract_address, this.rpc_url);
+        this.interface = new ERC20Interface(
+          this.contract_address,
+          this.rpc_url
+        );
         break;
       case "erc721":
-        this.interface = new ERC721Interface(this.contract_address, this.rpc_url);
+        this.interface = new ERC721Interface(
+          this.contract_address,
+          this.rpc_url
+        );
         break;
       case "erc1155":
-        this.interface = new ERC1155Interface(this.contract_address, this.rpc_url);
+        this.interface = new ERC1155Interface(
+          this.contract_address,
+          this.rpc_url
+        );
         break;
       case "custom":
         // If custom create and save model id
@@ -38,17 +48,17 @@ export default class EthereumEventPlugin {
         let model;
 
         try {
-          abi = JSON.parse(this.contract_abi)
+          abi = JSON.parse(this.contract_abi);
           events = getEventsFromABI(this.contract_abi);
-        } catch(e) {
-          console.log("error parsing abi:", e)
+        } catch (e) {
+          console.log("error parsing abi:", e);
         }
 
-        if(events) {
-          event = events.find(event => event.name == this.event_name);
+        if (events) {
+          event = events.find((event) => event.name == this.event_name);
         }
 
-        if(event) {
+        if (event) {
           // Generate custom schema for the selected event
           let properties = createJsonSchemaForEvent(event);
           schema = {
@@ -61,60 +71,82 @@ export default class EthereumEventPlugin {
             implements: [], // Example field for ModelDefinitionV2
             schema: properties,
           };
-          
+
           // Create model
           try {
             model = await this.orbisdb.ceramic.createModel(schema);
-          } catch(e) {
-              console.log(cliColors.text.red, "Error creating model:", cliColors.reset, e);
+          } catch (e) {
+            console.log(
+              cliColors.text.red,
+              "Error creating model:",
+              cliColors.reset,
+              e
+            );
           }
         }
 
-        if(model) {
-          this.interface = new CustomInterface(this.contract_address, this.rpc_url, abi, model?.id);
+        if (model) {
+          this.interface = new CustomInterface(
+            this.contract_address,
+            this.rpc_url,
+            abi,
+            model?.id
+          );
         }
-        
+
         break;
       default:
-        this.interface = new ERC20Interface(this.contract_address, this.rpc_url);
+        this.interface = new ERC20Interface(
+          this.contract_address,
+          this.rpc_url
+        );
         break;
     }
 
     // Subscribe to event selected by user
     try {
-      this.subscription = this.interface.contract?.events?.allEvents({ fromBlock: 'latest' });
-      this.subscription.on('data', this.handleEvent);
-      logger.debug("⭐️ Starting to listen to smart contract events:", this.contract_type);
-    } catch(e) {
-      logger.error("Error subscribing to event fron this smart contract:", this.contract_address);
+      this.subscription = this.interface.contract?.events?.allEvents({
+        fromBlock: "latest",
+      });
+      this.subscription.on("data", this.handleEvent);
+      this.subscription.on("error", console.error);
+      logger.debug(
+        "⭐️ Starting to listen to smart contract events:",
+        this.contract_type
+      );
+    } catch (e) {
+      logger.error(
+        "Error subscribing to event fron this smart contract:",
+        this.contract_address
+      );
     }
   }
 
-  stop() {
-    console.log("Trying to stop web3 subscription for:", this.uuid)
+  async stop() {
+    console.log("Trying to stop web3 subscription for:", this.uuid);
     try {
-      this.subscription.unsubscribe();
+      await this.subscription?.unsubscribe();
       console.log("Success unsubscribing from contract events.");
-    } catch(e) {
+    } catch (e) {
       console.log("Error unsubscribing from contract events:", e);
     }
   }
 
   // Function to handle detected events
   handleEvent = async (event) => {
-    if(event.event == this.event_name) {
+    if (event.event == this.event_name) {
       let _event = this.interface.parse(event);
-      console.log('Formatted event:', _event);
-  
+      console.log("Formatted event:", _event);
+
       // Insert event as a new stream
-      if(_event.model_id) {
+      if (_event.model_id) {
         try {
           let stream = await this.orbisdb
             .insert(_event.model_id)
             .value(_event.content)
             .context(this.context)
             .run();
-        } catch(e) {
+        } catch (e) {
           console.log("Error creating stream:", e);
         }
       }
@@ -122,13 +154,12 @@ export default class EthereumEventPlugin {
   };
 }
 
-
 function getEventsFromABI(abi) {
   let events = [];
   try {
     let abiJson = JSON.parse(abi);
-    events = abiJson.filter(item => item.type === 'event');
-  } catch(e) {
+    events = abiJson.filter((item) => item.type === "event");
+  } catch (e) {
     console.log("Error parsing ABI.");
   }
 
@@ -138,28 +169,37 @@ function getEventsFromABI(abi) {
 /** Will create the json schema for this event */
 function createJsonSchemaForEvent(event) {
   if (!event || !event.inputs) {
-      throw new Error('Invalid event object');
+    throw new Error("Invalid event object");
   }
 
   const schema = {
-      type: "object",
-      $schema: "https://json-schema.org/draft/2020-12/schema",
-      additionalProperties: false,
-      properties: {
-        _auto_type: { type: 'string', description: 'Automatically generated event name' },
-        _auto_contract: { type: 'string', description: 'Automatically generated contract address' },
-        _auto_hash: { type: 'string', description: 'Automatically generated transaction hash' }
+    type: "object",
+    $schema: "https://json-schema.org/draft/2020-12/schema",
+    additionalProperties: false,
+    properties: {
+      _auto_type: {
+        type: "string",
+        description: "Automatically generated event name",
       },
-      required: []
+      _auto_contract: {
+        type: "string",
+        description: "Automatically generated contract address",
+      },
+      _auto_hash: {
+        type: "string",
+        description: "Automatically generated transaction hash",
+      },
+    },
+    required: [],
   };
 
-  event.inputs.forEach(input => {
-      schema.properties[input.name] = {
-          type: getType(input.type),
-          description: input.internalType
-      };
-      // Assuming all inputs are required
-      schema.required.push(input.name);
+  event.inputs.forEach((input) => {
+    schema.properties[input.name] = {
+      type: getType(input.type),
+      description: input.internalType,
+    };
+    // Assuming all inputs are required
+    schema.required.push(input.name);
   });
 
   return schema;
@@ -168,19 +208,20 @@ function createJsonSchemaForEvent(event) {
 /** Will convert solidity types in json types */
 function getType(solidityType) {
   switch (solidityType) {
-      case 'address':
-          return 'string';
-      case 'uint8':
-      case 'int8':
-          return 'integer';
-      case 'bool':
-          return 'boolean';
-      case 'bytes':
-      case 'string':
-      case 'uint256':
-      case 'int256':
-          return 'string';
-      default:
-          return 'string'; // default type if not matched
+    case "address":
+      return "string";
+    case "uint8":
+    case "int8":
+      return "integer";
+    case "bool":
+      return "boolean";
+    case "bytes":
+    case "string":
+    case "uint256":
+    case "int256":
+      return "string";
+    default:
+      return "string"; // default type if not matched
   }
 }
+
